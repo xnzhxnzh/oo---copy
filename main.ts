424302bc9c99fb57a28286b9920bb04813a0fa0f
xnzhxnzh <35820138+xnzhxnzh@users.noreply.github.com>
2026-04-04 20:35:48 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,34 @@
 huskylens2.I2CInit()
 huskylens2.switchAlgorithm(huskylens2.Algorithm.AlgorithmFaceRecognition)
-I2C_LCD1602.LcdInit(2)
-I2C_LCD1602.on()
-I2C_LCD1602.BacklightOn()
 let servo_angle = 90
+let servo_angle_2 = 90
 servos.P0.setAngle(servo_angle)
+servos.P1.setAngle(servo_angle_2)
 basic.forever(function () {
+    huskylens2.getResultFaceRecognition()
     if (huskylens2.availableFaceRecognition()) {
         basic.showNumber(huskylens2.cachedCenterResult(huskylens2.BasePropertyId.XCenter))
     } else {
         basic.showIcon(IconNames.No)
     }
     huskylens2.getResultFaceRecognition()
-    if (huskylens2.cachedCenterResult(huskylens2.BasePropertyId.XCenter) < 300) {
-        servo_angle += 1
+    if (huskylens2.availableFaceRecognition()) {
+        if (huskylens2.cachedCenterResult(huskylens2.BasePropertyId.XCenter) < 240) {
+            servo_angle += 1
+        }
+        if (380 < huskylens2.cachedCenterResult(huskylens2.BasePropertyId.XCenter)) {
+            servo_angle += -1
+        }
+        servos.P0.setAngle(servo_angle)
+        if (huskylens2.cachedCenterResult(huskylens2.BasePropertyId.YCenter) < 180) {
+            servo_angle_2 += -1
+        }
+        if (260 < huskylens2.cachedCenterResult(huskylens2.BasePropertyId.YCenter)) {
+            servo_angle_2 += 1
+        }
+        servos.P1.setAngle(servo_angle_2)
+        basic.pause(5)
+    } else {
+        basic.showIcon(IconNames.No)
     }
-    if (340 < huskylens2.cachedCenterResult(huskylens2.BasePropertyId.XCenter)) {
-        servo_angle += -1
-    }
-    servos.P0.setAngle(servo_angle)
-    basic.pause(100)
 })
